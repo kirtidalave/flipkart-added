b16d7f1b806f7e9ff039a896e6e393d869d5547f
kirtidalave <dalavekirti@gmail.com>
2022-03-31 14:53:08 +0530

    flpikart added successfully

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+product=input("enter the product to be searched")
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.maximize_window()
+driver.get("https://www.flipkart.com")
+time.sleep(1)
+driver.find_element_by_xpath("").click()
+time.sleep(1)
+driver.find_element_by_name("q").send_keys(product)
+time.sleep(1)
+driver.find_element_by_class_name("L0Z3Pu").click()
+time.sleep(5)
+driver.close()
